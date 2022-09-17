e8d59397c890e32f460bd0820750ae20285957c2
Kim mingi <kmk2737@snu.ac.kr>
2022-09-17 20:53:36 +0900

    first commit

---
+++ study/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+# Create your views here.
+
+def study(request):
+    template = loader.get_template('study/study.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
