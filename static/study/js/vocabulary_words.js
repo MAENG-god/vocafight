556fa14428ca7a88954b5c607e5f37d610c1e89f
Kim mingi <kmk2737@snu.ac.kr>
2022-10-05 03:09:24 +0900

    add placeholder in edit

--- static/study/js/vocabulary_words.js
+++ static/study/js/vocabulary_words.js
@@ -1,28 +1,7 @@
-function vocaEdit(id, vocabularyId){
+function vocaEdit(id){
     var vocaClass = ".voca_" + id;
     var voca = document.querySelector(vocaClass);
     var editForm = document.querySelector(".hidden_voca_" + id)
     voca.remove()
-
-    var editEng = document.createElement('input');
-    var editKor = document.createElement('input');
-    var submitBtn = document.createElement('input');
-
-    editEng.className = 'eng'
-    editEng.setAttribute("type", "text");
-    editEng.setAttribute("name", "eng");
-
-    editKor.className = 'kor'
-    editKor.setAttribute("type", "text");
-    editKor.setAttribute("name", "kor");
-
-    submitBtn.className = 'btn submitBtn';
-    submitBtn.setAttribute("value", "확인");
-    submitBtn.setAttribute("type", "submit");
-
-    editForm.appendChild(editEng);
-    editForm.appendChild(editKor);
-    editForm.appendChild(submitBtn)
-
-    editForm.style.visibility = "visible";
+    editForm.style.display = "flex";
 }
